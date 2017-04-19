d57aa2b192d5158425a381f0600d62532890a9e4
Cbinbin <1142293069@qq.com>
2017-04-19 22:10:02 +0800

    log

--- 3rdwx/routes/wechat.js
+++ 3rdwx/routes/wechat.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
   , request = require('superagent')
 
-router.get('/', (req, res)=> {
+router.post('/', (req, res)=> {
   console.log(req.body)
   request.post('b671ad43.ngrok.io/wechat/accept')
   .send({weixin: req.body})
