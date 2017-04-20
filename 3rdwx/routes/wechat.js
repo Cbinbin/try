3f1f607c9d8d30735d1164633ad2ab2115de73a9
Cbinbin <1142293069@qq.com>
2017-04-20 11:11:27 +0800

    req

--- 3rdwx/routes/wechat.js
+++ 3rdwx/routes/wechat.js
@@ -1,9 +1,10 @@
 const router = require('express').Router()
   , request = require('superagent')
   , wxutil = require('../wxutil')
+  , token = process.env.TOKEN
 
 router.post('/', (req, res)=> {
-  var ssss = wxutil(req, token, req.body.xml.encrypt)
+  var ssss = wxutil(req.query.msg_signature, req.query.timestamp, req.query.nonce, token, req.body.xml.encrypt)
   console.log(ssss)
   request.post('103933be.ngrok.io/wechat/accept')
   .send({
@@ -21,6 +22,7 @@ router.post('/', (req, res)=> {
 
 router.post('/accept', (req, res)=> {
   console.log(req.body)
+  var sss = wxutil(req.body.msg_signature, req.body.timestamp, req.body.nonce, token, req.body.xml.encrypt)
 })
 
 module.exports = router
--- 3rdwx/wxutil.js
+++ 3rdwx/wxutil.js
@@ -2,11 +2,8 @@ const crypto = require('crypto')
   , url = require('url')
   , sha1 = crypto.createHash('sha1')
 
-function wxutil(req, token, msg_encrypt) {
-  var msg_signature = req.query.msg_signature
-    , timestamp = req.query.timestamp
-    , nonce = req.query.nonce
-    , dev_msg_signature = sha1(sort(token, timestamp, nonce, msg_encrypt))
+function wxutil(msg_signature, timestamp, nonce, token, msg_encrypt) {
+  var dev_msg_signature = sha1(sort(token, timestamp, nonce, msg_encrypt))
     , pass = false
   console.log(timestamp, nonce, dev_msg_signature, msg_signature)
   if(dev_msg_signature === msg_signature) pass = true
