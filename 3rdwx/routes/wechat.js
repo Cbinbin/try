0fa400f549edfabe8a7a1fac14679b9dc0b2376f
Cbinbin <1142293069@qq.com>
2017-04-21 16:30:14 +0800

    base64

--- 3rdwx/routes/wechat.js
+++ 3rdwx/routes/wechat.js
@@ -25,21 +25,26 @@ router.post('/', (req, res)=> {
 })
 
 router.post('/accept', (req, res)=> {
-  var ssss = wxutil(req.body.msg_signature, req.body.timestamp, req.body.nonce, token, req.body.xml.encrypt)
-  console.log(ssss)
-  var encrypt = req.body.xml.encrypt//'+KcSzmXyUTFAOqdtbgoUgCNXEu66Aevk3Q40mHTvjgtfErtVoBtiBUaNTBaTVRpmvB0eSn4RfT+XTGcD1h35mRS9hFl3dOE6+RbEN5S6dNd//VTP5gDkSMoScfh1HAocYriHu5HoXO9dXArAdfaM75xVO9fKZ5eS+g5RYSyCalcXy8HNmq0kPB/mcs4Y3AEf0lIfeSZloFFRb9sD9Z87duFR3mK3+H9PMZM5+w4uIV6r21x8Zex8EEvu2Jo+WwS8HVd4IBHCXWvfgNgXX4SX2tgI4f/A53UOsJYSv45JRGTnq0BBpV3JavI1eWgmt4cXUWac4pD5cyda/1LBuzUUG0PECJzMx8jySHqq4gi3WF9flMOS8HEOyRRcikXfdnlh7yScFsGD7jt7z2zcFIo46ThIbv7VylawxeouVr+1EiBFLDlz/3FpYDHAzdDHCKoIQrwOv6zniP2LcQvdwW45Rg=='
-    , AESKey = base64.decode(EncodingAESKey + '=')
-    , encrypt_dec = base64.decode(encrypt)
-    , iv = AESKey.substr(0, 16)
-  var decipher = crypto.createDecipheriv('aes-128-cbc', AESKey, iv)
-  // decipher.setAutoPadding(true)
-  var demsg = decipher.update(encrypt_dec, 'binary', 'utf8')
+  var encrypt = '+KcSzmXyUTFAOqdtbgoUgCNXEu66Aevk3Q40mHTvjgtfErtVoBtiBUaNTBaTVRpmvB0eSn4RfT+XTGcD1h35mRS9hFl3dOE6+RbEN5S6dNd//VTP5gDkSMoScfh1HAocYriHu5HoXO9dXArAdfaM75xVO9fKZ5eS+g5RYSyCalcXy8HNmq0kPB/mcs4Y3AEf0lIfeSZloFFRb9sD9Z87duFR3mK3+H9PMZM5+w4uIV6r21x8Zex8EEvu2Jo+WwS8HVd4IBHCXWvfgNgXX4SX2tgI4f/A53UOsJYSv45JRGTnq0BBpV3JavI1eWgmt4cXUWac4pD5cyda/1LBuzUUG0PECJzMx8jySHqq4gi3WF9flMOS8HEOyRRcikXfdnlh7yScFsGD7jt7z2zcFIo46ThIbv7VylawxeouVr+1EiBFLDlz/3FpYDHAzdDHCKoIQrwOv6zniP2LcQvdwW45Rg=='
+    , AESKey = new Buffer(EncodingAESKey + '=', 'base64')
+    // , encrypt_dec = new Buffer(encrypt, 'base64')
+    , iv = AESKey.slice(0, 16)
+  var decipher = crypto.createDecipheriv('aes-256-cbc', AESKey, iv)
+  decipher.setAutoPadding(false)
+  var deciphered = Buffer.concat([decipher.update(encrypt, 'base64'), decipher.final()])
+  var pad = deciphered[deciphered.length - 1];
+  if (pad < 1 || pad > 32) {
+      pad = 0;
+  }
+  deciphered.slice(0, deciphered.length - pad)
+
+  // var demsg = decipher.update(encrypt_dec, 'binary', 'utf8')
 
   // demsg = demsg.substring(16)
   // demsg += decipher.final('utf8')
   // var sss = wxutil(req.body.msg_signature, req.body.timestamp, req.body.nonce, token, req.body.xml.encrypt)
   // console.log(sss)
-  console.log(demsg)
+  res.send(deciphered)
 })
 
 module.exports = router
